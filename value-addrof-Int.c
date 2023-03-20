7f748f7d7425ff872cc750f7ce5046440905514f
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-20 19:36:29 +0530

    Add files via upload

---
+++ value-addrof-Int.c
@@ -0,0 +1,9 @@
+    // creating value and address
+     #include<stdio.h>
+ int main(){
+     int a;
+     printf("enter value a:");
+     scanf("%d",&a);
+     printf("the value of a is %d\n",a);
+     printf("the address of a is %p",a);
+ }
