16f49fa96135556e3cdcc12fed2635bbbbbbb9fb
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-23 21:40:48 +0530

    Add files via upload

---
+++ delete-last-node.c
@@ -0,0 +1,43 @@
+// delete last node
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+    
+};
+int main() {
+      struct node *new_node,*start,*temp1,*temp2;
+      int n;
+      scanf("%d",&n);
+       new_node=(struct node*)malloc(n*(sizeof(struct node)));
+      scanf("%d",&new_node->data);
+      
+      start=new_node;
+      temp1=new_node;
+      
+      for(int i=2;i<=n;i++){
+           new_node=(struct node*)malloc(n*(sizeof(struct node)));
+          scanf("%d",&new_node->data);
+          temp2=temp1;
+          temp1->next=new_node;
+          temp1=temp1->next;
+          }
+          
+           while(temp1->next!=NULL){
+               temp2=temp1;
+               temp1=temp1->next;
+               }
+               temp2->next=NULL;
+               free(temp1);
+           
+           struct node*temp;
+           
+          temp=start;
+          while(temp!=NULL){       //print data
+              printf("%d ",temp->data);
+              temp=temp->next;
+          }
+}
+          
