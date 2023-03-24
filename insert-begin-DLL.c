e98a1a0ac32a395387a163fa792c6a1b253535f5
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-24 08:58:30 +0530

    Add files via upload

---
+++ insert-begin-DLL.c
@@ -0,0 +1,54 @@
+//inserting node at begining in double linked list
+
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+    struct node *pre;
+    int data;
+    struct node *next;
+};
+
+
+int main(){
+    
+     struct node *new_node,*temp,*start;
+    int n;
+    scanf("%d",&n);
+    
+    new_node=(struct node*)malloc(n*sizeof(struct node));
+    scanf("%d",&new_node->data);
+    start=new_node;
+    temp=start;
+    new_node->pre=NULL;
+    
+    for(int i=2;i<=n;i++){
+        new_node=(struct node*)malloc(n*sizeof(struct node));
+        scanf("%d",&new_node->data);
+        temp->next=new_node;
+        new_node->pre=temp;
+        temp=temp->next;
+        }
+         
+       new_node=(struct node*)malloc(n*sizeof(struct node));
+    scanf("%d",&new_node->data);
+    
+    new_node->next=start;
+    start->pre=new_node;
+    new_node->pre=NULL;
+    start=new_node;
+    
+    temp=start;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    
+}
+
+/*4
+5 6 7 8
+7
+7 5 6 7 8 */
