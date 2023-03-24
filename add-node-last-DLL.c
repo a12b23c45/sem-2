bb168b969de2bba3fbf70d47ca7bbafc498dc4af
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-24 09:11:18 +0530

    Add files via upload

---
+++ add-node-last-DLL.c
@@ -0,0 +1,59 @@
+//Add node ast last in double linked list
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+    struct node *pre;
+    int data;
+    struct node *next;
+};
+
+
+int main(){
+    
+     struct node *new_node,*temp,*start;
+    int n;
+    scanf("%d",&n);
+    
+    new_node=(struct node*)malloc(n*sizeof(struct node));
+    scanf("%d",&new_node->data);
+    start=new_node;
+    temp=start;
+    new_node->pre=NULL;
+    
+    for(int i=2;i<=n;i++){
+        new_node=(struct node*)malloc(n*sizeof(struct node));
+        scanf("%d",&new_node->data);
+        temp->next=new_node;
+        new_node->pre=temp;
+        temp=temp->next;
+        }
+         
+     temp=start;
+     while(temp->next!=NULL)
+     {
+         temp=temp->next;
+     }
+     
+     new_node=(struct node*)malloc(n*sizeof(struct node));
+      scanf("%d",&new_node->data);
+      
+     temp->next=new_node;
+     new_node->pre=temp;
+     new_node->next=NULL;
+     
+    
+    temp=start;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    return 0;
+    
+}
+/*3
+4 5 6
+7
+4 5 6 7 */
