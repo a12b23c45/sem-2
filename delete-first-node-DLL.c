fff87e00e3c10d97aa676ac4aca94ac1e0b6ae26
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-24 09:25:12 +0530

    Add files via upload

---
+++ delete-first-node-DLL.c
@@ -0,0 +1,51 @@
+//Delete first node in double linked list
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+    struct node *pre;
+    int data;
+    struct node *next;
+};
+
+
+int main(){
+    
+     struct node *new_node,*temp,*start;
+    int n;
+    scanf("%d",&n);
+    
+    new_node=(struct node*)malloc(n*sizeof(struct node));
+    scanf("%d",&new_node->data);
+    start=new_node;
+    temp=start;
+    new_node->pre=NULL;
+    
+    for(int i=2;i<=n;i++){
+        new_node=(struct node*)malloc(n*sizeof(struct node));
+        scanf("%d",&new_node->data);
+        temp->next=new_node;
+        new_node->pre=temp;
+        temp=temp->next;
+        }
+         
+     temp=start;
+     start=temp->next;
+     temp->next->pre=NULL;
+     
+      
+     
+    
+    temp=start;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    return 0;
+    
+}
+/*3
+4 5 6
+5 6 */
